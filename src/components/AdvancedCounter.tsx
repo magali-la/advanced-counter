82b5594543b386ea67bd80b5f7a066b712be2f4f
Mag <166319982+magali-la@users.noreply.github.com>
2026-01-07 23:12:38 -0500

    fix(history): fix history counter printing values multiple times by using useEffect and setting default state value to an empty array for the first render with 0 to print only once

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -1,41 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function AdvancedCounter() {
     const [currentCount, setCurrentCount] = useState(0);
-    const [countHistory, setCountHistory] = useState<number[]>([0]);
+    const [countHistory, setCountHistory] = useState<number[]>([]);
 
     // handle increment
     function handleIncrement() {
         console.log('Increasing count');
         // use prev count and define varible to pass as argument to history function
-        setCurrentCount(prevCount => {
-            const newCount = prevCount + 1;
-            addToHistory(newCount);
-            // return new count as the value for current count state update
-            return newCount;
-        });
+        setCurrentCount(prevCount => prevCount + 1);
     }
 
     // handle decrement
     function handleDecrement() {
         console.log('Decreasing count');
-        setCurrentCount(prevCount => {
-            const newCount = prevCount - 1;
-            addToHistory(newCount);
-        
-            // return new count as the value for current count state update
-            return newCount;
-        })
+        setCurrentCount(prevCount => prevCount - 1)
     }
 
-    // add to history function
-    function addToHistory(newCount: number): void {
-        // update array using spread operator and functional update
-        setCountHistory(prevCountHistory => [...prevCountHistory, newCount]);
-    }
+    // create useEffect to listen for changes to count and add it to history
+    useEffect(() => {
+        setCountHistory(prevHistory => [...prevHistory, currentCount])
+    }, [currentCount])
 
     return (
-        <div className="py-6 px-4 bg-yellow-50 flex flex-col rounded-2xl border-2 border-green-100/60 gap-8">
+        <div className="py-6 px-4 bg-yellow-50 flex flex-col rounded-2xl border-2 border-green-100/60 gap-8 w-[30vw]">
             <h3>Current Count: {currentCount}</h3>
             {/* increment and decrement buttons */}
             <div className="flex flex-row gap-10 justify-center">
@@ -43,9 +31,10 @@ export default function AdvancedCounter() {
                 <button className="bg-green-300 hover:bg-linear-to-tr hover:from-green-400 hover:to-green-300" aria-label="increase button" role="button" onClick={handleIncrement}>Increase</button>
             </div>
             {/* history section */}
-            <div>
+            <div className="mx-10 max-w-full">
                 <h4>Count History</h4>
-
+                {/* add a p which will reflect the history array represented as a string joined by commas */}
+                <p>{countHistory.join(', ')}</p>
             </div>
             <button className="bg-yellow-200 hover:bg-linear-to-tr hover:from-yellow-500 hover:to-yellow-200 mx-6" aria-label="reset count button" role="button">Reset</button>
         </div>
